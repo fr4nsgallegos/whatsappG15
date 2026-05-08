3e7fec2312b53ed1f7c7a72a3a00f311e5c45111
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-05-07 19:47:29 -0500

    características de tabbar

--- lib/examples/tabbar_page.dart
+++ lib/examples/tabbar_page.dart
@@ -8,15 +8,26 @@ class TabbarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
+      length: 5, //número de pestañas
+      initialIndex: 1, //por defecto es 0, indica la pestaña inicial
       child: Scaffold(
         appBar: AppBar(
           title: Text("TabBar Page"),
           bottom: TabBar(
+            indicatorColor: Colors.red, //Color de la barra que se selecciona
+            indicatorWeight: 8, //ancho de la barra
+            unselectedLabelColor:
+                Colors.pink, // color de los label NO seleccionado (tab)
+            labelColor: Colors.cyan, // color del label seleccionado (tab)
+            isScrollable:
+                true, //si tienes muchas pestañas, te ayuda a desplazar
+            // conjunto de pestañas
             tabs: [
               Tab(text: "chats"),
               Tab(text: "estados"),
               Tab(text: "llamadas"),
+              Tab(text: "pestaña 4"),
+              Tab(text: "pestaña 5"),
             ],
           ),
         ),
@@ -25,6 +36,8 @@ class TabbarPage extends StatelessWidget {
             Center(child: Text("Pantalla chats")),
             Center(child: Text("Pantalla Estados")),
             Center(child: Text("Pantalla Llamadas")),
+            Center(child: Text("Pantalla 4")),
+            Center(child: Text("Pantalla 5")),
           ],
         ),
       ),
