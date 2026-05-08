b6806ad484ceaab472a1d609759b620081af1edf
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-05-07 22:48:08 -0500

    intro stack

--- lib/tabs/status_tab.dart
+++ lib/tabs/status_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsappg15/widgets/item_status_widget.dart';
 
 class StatusTab extends StatelessWidget {
   const StatusTab({super.key});
@@ -8,29 +9,63 @@ class StatusTab extends StatelessWidget {
     return Column(
       children: [
         ListTile(
-          leading: Container(
-            padding: EdgeInsets.all(2),
-            width: 58,
-            height: 58,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              shape: BoxShape.circle,
-              border: Border.all(color: Colors.grey, width: 3),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(80),
-              child: Image.network(
-                "https://images.pexels.com/photos/17734004/pexels-photo-17734004.jpeg",
-                fit: BoxFit.cover,
+          leading: Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.all(2),
+                width: 58,
+                height: 58,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(80),
+                  child: Image.network(
+                    "https://images.pexels.com/photos/17734004/pexels-photo-17734004.jpeg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  height: 25,
+                  width: 25,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.green,
+                    border: Border.all(color: Colors.white, width: 2),
+                  ),
+                  child: Icon(Icons.add, color: Colors.white, size: 15),
+                ),
+              ),
+            ],
           ),
           title: Text(
-            "Carlos",
+            "My status",
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
           ),
-          subtitle: Text("Ayer, 16:27", style: TextStyle(fontSize: 16)),
         ),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+          width: double.infinity,
+          child: Text(
+            "Actualizaciones Recientes",
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.green,
+            ),
+          ),
+        ),
+        ItemStatusWidget(),
+        ItemStatusWidget(),
+        ItemStatusWidget(),
+        ItemStatusWidget(),
+        ItemStatusWidget(),
       ],
     );
   }
---
+++ lib/widgets/item_status_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ItemStatusWidget extends StatelessWidget {
+  const ItemStatusWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        padding: EdgeInsets.all(2),
+        width: 58,
+        height: 58,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+          border: Border.all(color: Colors.grey, width: 3),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(80),
+          child: Image.network(
+            "https://images.pexels.com/photos/17734004/pexels-photo-17734004.jpeg",
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      title: Text(
+        "Carlos",
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+      ),
+      subtitle: Text("Ayer, 16:27", style: TextStyle(fontSize: 16)),
+    );
+  }
+}
