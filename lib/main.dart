6c26ef38d05632d4c8500617845b0c4093a2f660
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-05-07 19:13:40 -0500

    first

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:whatsappg15/pages/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+}
