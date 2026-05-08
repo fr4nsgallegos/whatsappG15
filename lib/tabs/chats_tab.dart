a8a90d94f4b54386ff38949eaabb722521e8975f
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-05-07 21:51:32 -0500

    pasando chat a stls

--- lib/tabs/chats_tab.dart
+++ lib/tabs/chats_tab.dart
@@ -1,53 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:whatsappg15/widgets/chat_widget.dart';
 
 class ChatsTab extends StatelessWidget {
   const ChatsTab({super.key});
 
-  ListTile _buildChatListile() {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundColor: Colors.red,
-        radius: 28,
-        backgroundImage: NetworkImage(
-          "https://images.pexels.com/photos/4329745/pexels-photo-4329745.jpeg",
-        ),
-      ),
-      title: Text("Nombre de la persona"),
-      titleTextStyle: TextStyle(
-        fontWeight: FontWeight.bold,
-        color: Colors.black,
-        fontSize: 16,
-      ),
-      subtitle: Text("¿Hola cómo estas?"),
-      trailing: Column(
-        children: [
-          Text("15:30", style: TextStyle(fontSize: 14)),
-          Container(
-            alignment: Alignment.center,
-            width: 25,
-            height: 25,
-            decoration: BoxDecoration(
-              color: Colors.green,
-              shape: BoxShape.circle,
-            ),
-            child: Text("1", style: TextStyle(color: Colors.white)),
-          ),
-        ],
-      ),
-    );
-  }
+  // ESTO ES UNA FUNCIÓN
+  // ListTile _buildChatListile() {
+  //   return ListTile(
+  //     leading: CircleAvatar(
+  //       backgroundColor: Colors.red,
+  //       radius: 28,
+  //       backgroundImage: NetworkImage(
+  //         "https://images.pexels.com/photos/4329745/pexels-photo-4329745.jpeg",
+  //       ),
+  //     ),
+  //     title: Text("Nombre de la persona"),
+  //     titleTextStyle: TextStyle(
+  //       fontWeight: FontWeight.bold,
+  //       color: Colors.black,
+  //       fontSize: 16,
+  //     ),
+  //     subtitle: Text("¿Hola cómo estas?"),
+  //     trailing: Column(
+  //       children: [
+  //         Text("15:30", style: TextStyle(fontSize: 14)),
+  //         Container(
+  //           alignment: Alignment.center,
+  //           width: 25,
+  //           height: 25,
+  //           decoration: BoxDecoration(
+  //             color: Colors.green,
+  //             shape: BoxShape.circle,
+  //           ),
+  //           child: Text("1", style: TextStyle(color: Colors.white)),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        _buildChatListile(),
-        _buildChatListile(),
-        _buildChatListile(),
-        _buildChatListile(),
-        _buildChatListile(),
-        _buildChatListile(),
-        _buildChatListile(),
+        ChatWidget(),
+        ChatWidget(),
+        ChatWidget(),
+        ChatWidget(),
+        ChatWidget(),
+        ChatWidget(),
+        ChatWidget(),
+        ChatWidget(),
+        ChatWidget(),
       ],
     );
   }
---
+++ lib/widgets/chat_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ChatWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: Colors.red,
+        radius: 28,
+        backgroundImage: NetworkImage(
+          "https://images.pexels.com/photos/4329745/pexels-photo-4329745.jpeg",
+        ),
+      ),
+      title: Text("Nombre de la persona"),
+      titleTextStyle: TextStyle(
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+        fontSize: 16,
+      ),
+      subtitle: Text("¿Hola cómo estas?"),
+      trailing: Column(
+        children: [
+          Text("15:30", style: TextStyle(fontSize: 14)),
+          Container(
+            alignment: Alignment.center,
+            width: 25,
+            height: 25,
+            decoration: BoxDecoration(
+              color: Colors.green,
+              shape: BoxShape.circle,
+            ),
+            child: Text("1", style: TextStyle(color: Colors.white)),
+          ),
+        ],
+      ),
+    );
+  }
+}
