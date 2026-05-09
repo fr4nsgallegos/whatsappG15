94c54c3d39ba4d04af264f36ef163de16145f1ad
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-05-08 20:31:56 -0500

    fuentes2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:whatsappg15/examples/tabbar_dinamico_page.dart';
 import 'package:whatsappg15/examples/tabbar_manual_page.dart';
 import 'package:whatsappg15/examples/tabbar_page.dart';
 import 'package:whatsappg15/pages/home_page.dart';
 
+// Ejemplo de fuente remota de manera general
 void main() {
-  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
+
+// Ejemplo de fuente local de manera general
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       theme: ThemeData(fontFamily: "Playwrite"),
+//       home: HomePage(),
+//       debugShowCheckedModeBanner: false,
+//     ),
+//   );
+// }
--- lib/tabs/camera_tab.dart
+++ lib/tabs/camera_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CameraTab extends StatelessWidget {
   const CameraTab({super.key});
@@ -12,6 +13,14 @@ class CameraTab extends StatelessWidget {
         Text(
           "Texto con una fuente cargada desde local, asset font",
           style: TextStyle(fontFamily: "Playwrite", fontSize: 30),
+        ),
+        Text(
+          "Texto con una fuente desde remoto, google font",
+          style: GoogleFonts.jaro(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "Texto con una fuente desde remoto, google font",
+          style: GoogleFonts.lato(fontSize: 30, fontWeight: FontWeight.normal),
         ),
       ],
     );
